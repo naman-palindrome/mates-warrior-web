7366d4e093c7fbc7196427f30880a2e3d0e391da
Simranjeet Singh <smrnjeet222@gmail.com>
2021-05-05 17:56:57 +0530

    login

--- src/views/pages/Auth/Login.jsx
+++ src/views/pages/Auth/Login.jsx
@@ -3,6 +3,7 @@ import { FormControl, FormLabel } from '@chakra-ui/form-control'
 import { PhoneIcon } from '@chakra-ui/icons'
 import { Image } from '@chakra-ui/image'
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input'
+import { Flex } from '@chakra-ui/layout'
 import { Box, Center, Heading, Stack, Text } from '@chakra-ui/layout'
 import React, { useState } from 'react'
 import LoginSVG from '../../../assets/login.svg'
@@ -15,50 +16,43 @@ function Login() {
 
   return (
     <Center minH="100vh">
-      <Box w="lg" borderWidth="1px" borderRadius="lg" minH="90vh"
-        className="bg-white px-6 pb-6"
+      <Box w='75vw' borderWidth="1px" borderRadius="lg" minH="80vh"
+        bgColor="#fff"
       >
-        <Image src={LoginSVG}
-          boxSize="300px"
-          objectFit="cover"
-          className="mx-auto"
-        />
-        <form onSubmit={handleSubmit}>
-          <FormControl id="mobile" className="my-6" isRequired>
-            <FormLabel className="text-gray-600">Enter Mobile Number</FormLabel>
-            <InputGroup
-              className="my-2"
-            >
-              <InputLeftElement
-                pointerEvents="none"
-                children={<PhoneIcon color="gray.300" />}
-              />
-              <Input size="md" variant="filled"
-                type="tel"
-                placeholder="+91 xxxxxxxxx"
-              />
-            </InputGroup>
-            <Button
-              width="100%"
-              colorScheme="yellow"
-              type='submit'
-              className="my-6"
-            >
-              Get OTP
-          </Button>
-          </FormControl>
-        </form>
-        <Stack spacing={0} className="pt-14">
-          <Text fontSize="md" className='mx-auto mt-auto'>
-            Initiative By
-          </Text>
-          <Image
-            src={MaitLogo}
-            boxSize="60px"
-            objectFit="container"
-            className='mx-auto'
-          />
-        </Stack>
+        <Flex>
+          <Center w='50%'>
+            <Image src={LoginSVG}
+              boxSize="300px"
+              objectFit="cover"
+            />
+          </Center>
+
+          <Box flex="1" p="4rem">
+            <form onSubmit={handleSubmit}>
+              <FormControl id="mobile" isRequired>
+                <FormLabel >Enter Mobile Number</FormLabel>
+                <InputGroup >
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<PhoneIcon color="gray.300" />}
+                  />
+                  <Input size="md" variant="filled"
+                    type="tel"
+                    placeholder="+91 xxxxxxxxx"
+                  />
+                </InputGroup>
+                <Button
+                  my="1rem"
+                  width="100%"
+                  colorScheme="yellow"
+                  type='submit'
+                >
+                  Get OTP
+              </Button>
+              </FormControl>
+            </form>
+          </Box>
+        </Flex>
       </Box>
     </Center>
   )
